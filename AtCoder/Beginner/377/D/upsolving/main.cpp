bdcd3818b71910f94ce3ce20c3f27116d353a83e
MarcosViniciusG <marcos.vinicius.gundel.silva@gmail.com>
2024-11-03 11:37:03 -0300

    AtCoder 377D upsolving added

--- AtCoder/Beginner/377/D/upsolving/main.cpp
+++ AtCoder/Beginner/377/D/upsolving/main.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main() {
+    int n, m, li, ri;
+    cin >> n >> m;
     
+    // For every r in [1, m], there is a number
+    // dr that every pair of integers in (dr, r)
+    // satisfies the condition
 
+    vector<int> d(m+1, 1);
+    for(int i=0; i<n; i++) {
+        cin >> li >> ri;
+        d[ri] = max(d[ri], li+1);
+    }
+
+    for(int i=1; i<=m; i++) {
+        d[i] = max(d[i], d[i-1]);
+    }
+
+    long long soma=0;
+    for(int i=1; i<=m; i++)
+        soma += (i - d[i]+1);
+
+    cout << soma << '\n';
     return 0;
 }

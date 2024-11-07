30cc08c851363ce61a2b439f9df9ffc5652c355b
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2024-11-07 20:02:43 -0300

    New solved problems

--- TEP/lista1/Permalex/main.cpp
+++ TEP/lista1/Permalex/main.cpp
@@ -2,38 +2,52 @@
 
 using namespace std;
 using ll = long long;
+#define endl '\n'
 
 vector<char> ranking;
+
 ll n_perms(int pos, char current) {
-    ll produto=1;
-    int n_elements=0;
-    int n_repeats=0;
-    set<char> st;
+    map<char, ll> already;
+    ll j=0;
+    ll soma=0;
 
+    while(ranking[j]!=current) {
+        ll produto=1;
+        int n_elements=0;
+        char counters[26] = {0};
 
-    for(int i=pos+1; i<ranking.size(); i++) {
-        if(st.find(ranking[i])!=st.end())
-            n_repeats++;
-        else
-            st.insert(ranking[i]);
-
-        n_elements++;
-    }
-
-    for(int i=n_elements; i>=1; i--) {
-        produto *= n_elements;
-        if(n_elements==n_repeats) {
-            produto /= n_repeats;
-            n_repeats--;
+        for(int i=pos+1; i<ranking.size(); i++) {
+            counters[ranking[i]-'a']++;
+            n_elements++;
         }
+        
+        // Fatorial
+        for(int i=n_elements; i>=1; i--) {
+            produto *= i;
+
+            // Divisão
+            for(int j=0; j<26; j++) {
+                if(i==counters[j]) {
+                    produto /= counters[j];
+                    counters[j]--;
+                }
+            }
+        }
+
+        if(already.find(ranking[j])==already.end())
+            soma += produto;
+            
+        already[ranking[j]] = produto;
+
+        j++;
     }
+    return soma;
 }
 
 int main() {
     string s;
     getline(cin, s);
     int n = s.length();
-    int countx=0;
 
     string ordem = s;
     sort(ordem.begin(), ordem.end());
@@ -42,9 +56,15 @@ int main() {
         ranking.push_back(ordem[i]);        
     }
 
-    for(int i=0; i<n; i++) {
-        cout << (i+1) << " " << ranking[i] << '\n';        
+    // for(int i=0; i<n; i++) {
+    //     cout << (i+1) << " " << ranking[i] << '\n';        
+    // }
+    ll soma=0;
+    for(ll i=0; i<n; i++) {
+        ll result = n_perms(i, s[i]);
+        cout << result << '\n';
+        soma += result;
     }
 
-    cout << (countx+1) << '\n';
+    cout << soma << '\n';
 }

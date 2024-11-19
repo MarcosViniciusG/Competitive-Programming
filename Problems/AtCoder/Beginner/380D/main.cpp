bb8e943a6f5602800baf4fc5f4a25851d4b8792c
MarcosViniciusG <marcos.vinicius.gundel.silva@gmail.com>
2024-11-19 16:44:07 -0300

    Upsolving 380D

--- Problems/AtCoder/Beginner/380D/main.cpp
+++ Problems/AtCoder/Beginner/380D/main.cpp
@@ -3,11 +3,31 @@
 using namespace std;
 using ll = long long;
 
-ll solve(ll k_th_block) {
+char flip(char c){
+  if('a'<=c && c<='z'){
+    return (c-'a')+'A';
+  }
+  else{
+    return (c-'A')+'a';
+  }
+}
+
+bool solve(ll k_th_block) {
   if(k_th_block==0)
-    return 0;
+    return false;
   
-  return ~solve(k_th_block-1);
+  ll msb = 0;
+  ll n = k_th_block / 2;
+    while (n != 0) {
+        n = n / 2;
+        msb++;
+    }
+
+  bool result = solve(k_th_block - (ll(1) << msb));
+  if(result)
+    return false;
+  else
+    return true;
 }
 
 signed main() {
@@ -15,13 +35,20 @@ signed main() {
   getline(cin, s);
 
   ll q, k, n=s.length();
+  cin >> q;
   while(q--) {
     cin >> k;
+    k--;
     ll k_th_block = (k/n);
-    ll idx = k%n;
-
-
+    char c = s[k%n];
+    bool cas = solve(k_th_block);
+    if(cas)
+      cout << flip(c) << ' ';
+    else
+      cout << c << ' ';
+    
   }
+  cout << '\n';
 
   return 0;
 }

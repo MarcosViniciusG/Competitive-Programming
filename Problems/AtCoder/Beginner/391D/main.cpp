9107ec4c25361c69d2977ca7a5b02c6a2017626b
MarcosViniciusG <marcos.vinicius.gundel.silva@gmail.com>
2025-03-07 20:51:30 -0300

    upsolving ABC 391D

--- Problems/AtCoder/Beginner/391D/main.cpp
+++ Problems/AtCoder/Beginner/391D/main.cpp
@@ -7,27 +7,45 @@ using vll = vector<ll>;
 using pll = pair<ll, ll>;
 using vpll = vector<pll>;
 
-#define endl '\n';
+#define endl '\n'
+#define INF 10000000000
 
 signed main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     ll N, W, x, y;
     cin >> N >> W;
-    set<ll> bottom_row;
-    set<ll> bottom_row_pos;
+    vector<vpll> cols (W);
+    vll d(N+1, 0);
     for(ll i=0; i<N; i++) {
         cin >> x >> y;
         x--; y--;
-        if(!y && bottom_row_pos.find(x)==bottom_row_pos.end()) {
-            bottom_row.insert(i);
-            bottom_row_pos.insert(x);
-        }
+        cols[x].push_back({y, i}); // Store y coord with the ID of each block
     }
 
+    ll blocks[N]; // Array to store y coord of each block
+    for(ll i=0; i<W; i++) {
+        sort(cols[i].begin(), cols[i].end()); // Sort all y coordinates in each col
+        for(ll c=0; c<ll(cols[i].size()); c++) {
+            blocks[cols[i][c].second] = c; // The block A is at the c-th pos in the col
+            d[c] = max(d[c], cols[i][c].first); // Max y coordinate
+        }
+        d[ll(cols[i].size())] = INF; // If the column doesn't have more than C blocks
+    }
+
+    for(ll c=1; c<=N; c++) 
+        d[c] = max(d[c], d[c-1] + 1); // Max between max coord and previous blocks
+
     ll Q, t, a;
+
     cin >> Q;
     for(ll j=0; j<Q; j++) {
         cin >> t >> a;
+        a--;
+        t--;
+        if(t < d[blocks[a]])
+            cout << "Yes\n";
+        else
+            cout << "No\n";
     }
 
     return 0;

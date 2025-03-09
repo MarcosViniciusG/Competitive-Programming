664d0b2e297920ba5361a13a92c5438c1f277ff2
MarcosViniciusG <marcos.vinicius.gundel.silva@gmail.com>
2025-03-09 20:04:27 -0300

    Upsolving 387D

--- Problems/AtCoder/Beginner/387C/main.cpp
+++ Problems/AtCoder/Beginner/387C/main.cpp
@@ -9,60 +9,53 @@ using vpll = vector<pll>;
 
 #define endl '\n';
 
-ll counting(ll x) {
-    string place="";
-    string xs = to_string(x);
-    place += xs[0];
-    for(ll i=0; i<ll(xs.length())-1; i++) {
-        place += '0';
-    }
-
-    ll smallest_n = stoll(place);
-    
-    ll result=0;
-    for(ll i=1; i<=9; i++) {
-        for(ll j=10; (j*i)<smallest_n; j*=10) {
-            ll pos=1;
-            ll place=j/10;
-            while(place > 0) {
-                pos *= i;
-                place /= 10;
-            }
-            result += pos;
-        }
-    }
-
-    place="";
-    place += xs[0];
-    bool activate=false;
-    for(ll i=1; i<ll(xs.length()); i++) {
-        if(xs[i] < xs[0] && !activate)
-            place += xs[i];
-        else {
-            activate = true;
-            place += xs[0]-1;
-        }
-    }
-
-    ll prod=1;
-    cout << place << '\n';
-    for(ll i=1; i<ll(place.length()); i++) {
-        prod *= (place[i]-'0')+1;
-    }
-    cout << prod << ' ' << result << '\n';
-
-    return result + prod;
+ll llpow(ll a, ll t) {
+    ll res = 1;
+    for (ll i = 0; i < t; i++) res *= a;
+    return res;
 }
 
-signed main() {
+ll counting(ll x)
+{
+    vll digit;
+    while(x) {
+        digit.push_back(x % 10);
+        x /= 10;
+    }
+
+    reverse(digit.begin(), digit.end());
+    ll n = digit.size();
+    ll res=0;
+    for(ll i=1; i<=n; i++) {
+        if(i==n) {
+            res++;
+            break;
+        }
+
+        res += llpow(digit[0], n-1-i) * min(digit[0], digit[i]);
+        if(digit[i] >= digit[0]) break;
+    }
+
+    for(ll i=0; i<n; i++) {
+        ll limit = (i ? 9: digit[0] - 1);
+        for(ll j=1; j<=limit; j++) {
+            res += llpow(j, n-1-i);
+        }
+    }
+
+    return res;
+}
+
+signed main()
+{
     cin.tie(nullptr)->sync_with_stdio(false);
+
     ll L, R;
     cin >> L >> R;
 
     ll resultR = counting(R);
-    ll resultL = counting(L);
-    
+    ll resultL = counting(L-1);
 
-    cout << resultR - resultL + 1 << '\n';
+    cout << resultR - resultL << '\n';
     return 0;
 }
--- Problems/AtCoder/Beginner/387D/main.cpp
+++ Problems/AtCoder/Beginner/387D/main.cpp
@@ -10,38 +10,80 @@ using vpll = vector<pll>;
 #define endl '\n';
 
 char grid[1001][1001];
-int dp[1001][1001][2] = {-1};
 
 int H, W;
+#define INF ll(1e9)
 
-int solve(int i, int j, bool horizontal) {
-    if(grid[i][j]=='G')
-
-    if(horizontal) {
-        if((j+1 < W) && dp[i][j+1]==-1)
-            dp[i][j+1] = 1 + solve(i, j+1, false);
-        if((j-1 >= 0) && dp[i][j-1]==-1)
-            dp[i][j-1] = 1 + solve(i, j-1, false); 
-    }
-    else {
-        solve(i+1, j, true);
-        solve(i-1, j, true);
-    }
-
-    if(horizontal)
-    {
-        if((j+1 < W) && (j-1 >=0))
-            return min(dp[i][j+1], dp[i][j-1]);
-        else if(j+1<W)
-            return dp[i][j+1];
-        else if(j-1>=0)
-
-    }
-        return min(dp[i])
-}
+ll sol = INF;
 
 signed main() {
     cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> H >> W;
+
+    ll starti, startj;
+    ll goali, goalj;
+    for(ll i=0; i<H; i++) {
+        for(ll j=0; j<W; j++) {
+            cin >> grid[i][j];
+            if(grid[i][j]=='S') {
+                starti = i;
+                startj = j;
+            }
+            else if(grid[i][j]=='G') {
+                goali = i;
+                goalj = j;
+            }
+        }
+    }
+
+
+    queue<pll> q;
+    bool horizontal;
+    ll dp[1001][1001];
+
+    for(ll k=0; k<2; k++) {
+        for(ll i=0; i<1001; i++) 
+        for(ll j=0; j<1001; j++)
+            dp[i][j] = INF;
+
+        horizontal = k; 
+        q.push({starti, startj});
+        dp[starti][startj] = 0;
+
+        // BFS to find shortest path to the goal
+        while (!q.empty())
+        {
+            pll u = q.front(); q.pop();
+            ll i = u.first;
+            ll j = u.second;
+
+            if(horizontal && !(dp[i][j] & 1) || (!horizontal && (dp[i][j] & 1))) {
+                if((j+1 < W) && (grid[i][j+1] != '#') && (dp[i][j+1]==INF)) {
+                    dp[i][j+1] = dp[i][j] + 1;
+                    q.push({i, j+1});
+                }
+
+                if((j-1 >= 0) && (grid[i][j-1] != '#') && (dp[i][j-1]==INF)) {
+                    dp[i][j-1] = dp[i][j] + 1;
+                    q.push({i, j-1});
+                }
+            }
+            else {
+                if((i+1 < H) && (grid[i+1][j] != '#') && (dp[i+1][j]==INF)) {
+                    dp[i+1][j] = dp[i][j] + 1;
+                    q.push({i+1, j});
+                }
+
+                if((i-1 >= 0) && (grid[i-1][j] != '#') && (dp[i-1][j]==INF)) {
+                    dp[i-1][j] = dp[i][j] + 1;
+                    q.push({i-1, j});
+                }
+            }
+        }
+        sol = min(sol, dp[goali][goalj]);
+    }
+
+    cout << (sol==INF ? -1 : sol) << '\n';
 
     return 0;
 }

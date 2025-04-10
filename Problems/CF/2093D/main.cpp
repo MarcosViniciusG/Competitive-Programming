0e958da6112a53141a0f0778b4a0f108f112531d
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-04-10 17:27:46 -0300

    upsolving CF2093

--- Problems/CF/2093D/main.cpp
+++ Problems/CF/2093D/main.cpp
@@ -21,47 +21,66 @@ signed main() {
             if(cmd=="->") {
                 ll x, y;
                 cin >> x >> y;
-                cout << "AINDA NAO FOI" << '\n';
-            }
-            else if(cmd=="<-") {
-                ll d;
-                cin >> d;
 
-                ll ord = n;
-                ll mx = (1 << (2*ord));
-                ll offset=1;
-
-                ll il = 0;
-                ll ir = 1 << n;
-                ll jl = 0;
-                ll jr = 1 << n;
-                while(ir-il!=0 && jr - jl != 0) {
-                    ll imed = (il + ir) / 2;
-                    ll jmed = (jl + jr) / 2;
-
-                    if(d <= mx/4) {
-                        ir = imed ;
-                        jr = jmed;
-                        mx = mx/4;
+                ll ord=n;
+                ll ans=1;
+                ll hfx = (1LL << (n-1));
+                ll hfy = (1LL << (n-1));                
+                while(ord>0) {
+                    ll v = (1LL << (2*ord - 2));
+                    ord--;
+                    if(x > hfx) {
+                        hfx += (1LL << (ord - 1));
+                        if(y > hfy) {
+                            ans += v;
+                            hfy += (1LL << (ord - 1)); 
+                        }
+                        else { 
+                            ans += v*2;
+                            hfy -= (1LL << (ord -1));
+                        }
                     }
-                    else if(d <= (mx/4)*2) {
-                        il = imed;
-                        jl  = jmed;
-                        mx = (mx/4)*2;
-                    }
-                    else if(d <= (mx/4)*3) {
-                        il = imed;
-                        jr = jmed;
-                        mx = (mx/4)*3;
-                    }
-                    else if(d <= mx) {
-                        ir =imed;
-                        jl = jmed ;
+                    else {
+                        hfx -= (1LL << (ord -1));
+                        if(y > hfy) {
+                            hfy += (1LL << (ord-1));
+                            ans += v*3;
+                        }
+                        else 
+                            hfy -= (1LL << (ord-1));
                     }
                 }
+                
+                cout << ans << "\n";
+            } 
+            else {
+                ll d;
+                cin >> d;
+                ll x=1, y=1;
 
-                cout << il << ' ' << jl << '\n';
-            }
+                ll ord=n;
+                while(ord > 0) {
+                    ll v =(1LL << (2*ord - 2));
+                    if(d <= v) {
+
+                    }
+                    else if(d <= v*2) {
+                        d -= v;
+                        x += (1LL << (ord-1));
+                        y += (1LL << (ord-1));
+                    }
+                    else if(d <= v*3) {
+                        d -= v*2;
+                        y += (1LL << (ord-1));
+                    }
+                    else if(d <= v*4) {
+                        d -= v*3;
+                        x += (1LL << (ord-1));
+                    }
+                    ord--;
+                }
+                cout << y << ' ' << x << '\n';
+            }         
         }
     }
 

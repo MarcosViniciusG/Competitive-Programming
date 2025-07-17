c68ab9a3523f27e28c8c856f1d7b0deb23c6d44c
Pedro Teixeira <127347431+PedroDev-sketch@users.noreply.github.com>
2025-07-17 02:31:20 -0300

    Add files via upload
    Added the element recuperation version of LIS

--- Algorithms/DynamicProgramming/lis/main.cpp
+++ Algorithms/DynamicProgramming/lis/main.cpp
@@ -1,19 +1,39 @@
-ll lis(vll const& a) {
-    ll n = a.size();
-    const ll INF = 1e9;
-    vll d(n+1, INF);
-    d[0] = -INF;
+vector<int> LIS(int N, const vector<int>& xs)
+{
+    vector<int> lis(N, 1), ps(N, -1);
 
-    for (ll i = 0; i < n; i++) {
-        ll l = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
-        if (d[l-1] < a[i] && a[i] < d[l])
-            d[l] = a[i];
+    for(int i = 1; i < N; i++)
+    {
+        for(int j = i - 1; j >= 0; j--)
+        {
+            if(xs[i] > xs[j] and lis[j] + 1 > lis[i])
+            {
+                lis[i] = lis[j] + 1;
+                ps[i] = j;
+            }
+        }
     }
 
-    ll ans = 0;
-    for (ll l = 0; l <= n; l++) {
-        if (d[l] < INF)
-            ans = l;
+    int best = 0, k = -1;
+
+    for(int i = 0; i < N; i++)
+    {
+        if(lis[i] > best)
+        {
+            best = lis[i];
+            k = i;
+        }
     }
+
+    vector<int> ans;
+
+    do
+    {
+        ans.emplace_back(xs[k]);
+        k = ps[k];
+    } while(k != -1);
+
+    reverse(ans.begin(), ans.end());
+
     return ans;
 }

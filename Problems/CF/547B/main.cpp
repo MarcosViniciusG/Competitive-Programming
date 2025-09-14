fff0f2bf47352d936ffb95a71f9a9f230a1fb356
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-09-14 18:03:39 -0300

    CF 547B

--- Problems/CF/547B/main.cpp
+++ Problems/CF/547B/main.cpp
@@ -1,3 +1,6 @@
+// O(n)
+// monotonic stack (smaller and greater previous element) and partial sum
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,11 +25,55 @@ signed main() {
     vll a(n);
     vll ans(n);
     stack<pll> st;
+
+    vll pse(n);
     for(ll i=0; i<n; i++) {
         cin >> a[i];
         
+        while(!st.empty() && st.top().first >= a[i])
+            st.pop();
+        
+        pse[i] = (st.empty() ? 0 : st.top().second+1);
+        
+        st.push({a[i], i});
     }
 
+    vll nse(n);
+    while(!st.empty()) st.pop();
 
+    for(ll i=n-1; i>=0; i--) {        
+        while(!st.empty() && st.top().first >= a[i])
+            st.pop();
+        
+        nse[i] = (st.empty() ? n-1 : st.top().second);
+        
+        st.push({a[i], i-1});
+    }
+
+    // for(auto e: pse)
+    //     cout << e << ' ';
+    // cout << '\n';
+
+    // for(auto e: nse)
+    //     cout << e << ' ';
+    // cout << '\n';
+
+    for(ll i=0; i<n; i++) {
+        ll sz = nse[i] - pse[i];
+        ans[sz] = max(ans[sz], a[i]);
+        // cout << sz << ' ';
+    }
+    // cout << "\n";
+
+    ll mx=0;
+    for(ll i=n-1; i>=0; i--) {
+        mx = max(mx, ans[i]);
+        ans[i] = mx;
+    }
+
+    for(auto e: ans)
+        cout << e << ' ';
+
+    cout << '\n';
     return 0;
 }
